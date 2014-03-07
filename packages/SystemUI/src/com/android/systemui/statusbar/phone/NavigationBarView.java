edd4650f1270538ac779cc9394812e0417ed83c4
Janson Kang <temasek71@gmail.com>
2014-03-07 16:33:07 +0800

    Revert "[2/2] Left handed navbar during landscape mode"
    This reverts commit a6fb5ad96c5531615930d46f0de70501bc5cb401.
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -87,7 +87,6 @@ public class NavigationBarView extends LinearLayout {
     int mBarSize;
     boolean mVertical;
     boolean mScreenOn;
-    boolean mLeftInLandscape;
 
     boolean mShowMenu;
     int mDisabledFlags = 0;
@@ -233,7 +232,6 @@ public class NavigationBarView extends LinearLayout {
         mBarSize = res.getDimensionPixelSize(R.dimen.navigation_bar_size);
         mVertical = false;
         mShowMenu = false;
-        mLeftInLandscape = false;
         mDelegateHelper = new DelegateViewHelper(this);
 
         getIcons(res);
@@ -639,11 +637,6 @@ public class NavigationBarView extends LinearLayout {
         return mVertical;
     }
 
-    public void setLeftInLandscape(boolean leftInLandscape) {
-        mLeftInLandscape = leftInLandscape;
-        mDeadZone.setStartFromRight(leftInLandscape);
-    }
-
     public void reorient() {
         int orientation = mContext.getResources().getConfiguration().orientation;
         mRotatedViews[Configuration.ORIENTATION_PORTRAIT].setVisibility(View.GONE);
@@ -661,7 +654,6 @@ public class NavigationBarView extends LinearLayout {
         updateSettings();
 
         mDeadZone = (DeadZone) mCurrentView.findViewById(R.id.deadzone);
-        mDeadZone.setStartFromRight(mLeftInLandscape);
 
         // force the low profile & disabled states into compliance
         mBarTransitions.init(mVertical);
@@ -807,4 +799,5 @@ public class NavigationBarView extends LinearLayout {
         }
         pw.println();
     }
+
 }
